3aa0a836c0444c3b381bd48b6a24d9616c5ee0f6
GregoryPartridgeSweng <72704856+GregoryPartridgeSweng@users.noreply.github.com>
2020-11-11 17:52:18 +0000

    Update LowestCommonAncestor.java

--- LowestCommonAncestor.java
+++ LowestCommonAncestor.java
@@ -1,8 +1,36 @@
 
-public class LowestCommonAncestor {
+class Node  
+{ 
+    int data; 
+    Node left, right; 
+   
+    Node(int item)  
+    { 
+        data = item; 
+        left = right = null; 
+    } 
+} 
 
-	public static void main(String[] args) {
-
+public class LowestCommonAncestor 
+{
+	Node root;
+	
+	Node findLowestCommonAncestor(int x, int y)
+	{
+		return findLowestCommonAncestor(root, x, y);
+	}
+	
+	Node findLowestCommonAncestor(Node node, int x, int y)
+	{
+		if(node == null) return null;
+		if(node.data == x || node.data == y) return node;
+		
+		Node lowestCommonAncestorLeft = findLowestCommonAncestor(node.left, x, y);
+		Node lowestCommonAncestorRight = findLowestCommonAncestor(node.right, x, y);
+		
+		if(lowestCommonAncestorLeft != null && lowestCommonAncestorRight != null) return node;
+		else if(lowestCommonAncestorLeft != null) return lowestCommonAncestorLeft;
+		else return lowestCommonAncestorRight;
 	}
 
 }
