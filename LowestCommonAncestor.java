9799a279716492e6abc03c5007a5b6380d9a597d
GregoryPartridgeSweng <72704856+GregoryPartridgeSweng@users.noreply.github.com>
2020-12-12 17:32:12 +0000

    Add files via upload

--- LowestCommonAncestor.java
+++ LowestCommonAncestor.java
@@ -1,107 +1,123 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
 
-import java.io.*; 
-import java.util.*; 
+public class LowestCommonAncestor {
 
-class Node  
-{ 
-    static int data;
-   
-    Node(int data)  
-    {  
-        data = data;
-    } 
-} 
+	int vertexTotal;
+	int Y;
+	ArrayList<Integer>[] adjacent;
+	int[] edgesToVertex;
+	boolean[] marked;
+	boolean[] stack;
+	boolean DAG; 
 
-class Graph
-{
-	int length;
-	ArrayList[] adj;
-	Node[] listOfNodes;
-	int currentNode;
-	
-	Graph(int length)
-	{
-		length = length;
-		currentNode = 0;
-		listOfNodes = new Node[length];
-		adj = new ArrayList[length]; 
-        for (int i=0; i < length; ++i) 
-        {
-            adj[i] = new ArrayList<Node>();
-        }
-	}
-	
-	void addNode(Node node)
-	{
-		listOfNodes[currentNode++] = node;
-	}
-	
-	int getSize(int index)
-	{
-		return adj[index].size();
-	}
-	
-	Node getEdge(int index, int edge)
-	{
-		return (Node) adj[index].get(edge);
-	}
-	
-	void addEdge(int parentNode, int data) 
-    { 
-		if(parentNode >= data) {
-			System.out.println("Error");
-			return;
-		}
-        Node node = new Node(data); 
-        adj[parentNode].add(node);
-        addNode(node);
-    }
-}
-
-public class LowestCommonAncestor 
-{
-	boolean[] foundXAndY;
-	
-	Node findLowestCommonAncestor(int x, int y, Graph adj)
-	{
-		return findLowestOneByOne(x, y, adj);
-	}
-	
-	Node findLowestOneByOne(int x, int y, Graph adj)
-	{
+	public LowestCommonAncestor(int vertexTotal) 
+	{ 
+		this.vertexTotal = vertexTotal;
+		this.Y = 0;
 		
-		Node currentAnswer = null;
-		foundXAndY = new boolean[2];
+		edgesToVertex = new int[vertexTotal];
+		adjacent = (ArrayList<Integer>[]) new ArrayList[vertexTotal];
+		for (int v = 0; v < vertexTotal; v++) adjacent[v] = new ArrayList<Integer>();
 		
-		for(int i = 0; i <= x || i <= y ; i++)
+		DAG = true;
+		marked = new boolean[vertexTotal];
+		stack = new boolean[vertexTotal];
+	}
+
+	public void addEdge(int vertex1, int vertex2) {
+		if(vertex1 < 0 || vertex2 < 0 || vertex1 >= vertexTotal || vertex2 >= vertexTotal) return;
+		adjacent[vertex1].add(vertex2);
+		edgesToVertex[vertex2]++;
+		Y++;
+	}
+
+	public void acyclic(int vertex) {
+		stack[vertex] = true;
+		marked[vertex] = true;
+
+		for (int counter : adjacent[vertex]) 
 		{
-			Arrays.fill(foundXAndY, Boolean.FALSE);
-			Node currNode = adj.listOfNodes[i];
-			findLowestCommonAncestor(currNode, x, y, adj);
-			if(foundXAndY[0] && foundXAndY[1]) currentAnswer = currNode;
+			if (!marked[counter]) acyclic(counter);
+			else if (stack[counter])
+			{
+				DAG = false;
+				return;
+			}
 		}
-		
-		return currentAnswer;
+		stack[vertex] = false;
+	}
+
+	public void isAcyclic() {
+		for (int i = 0; i < vertexTotal && DAG; i++) {
+			stack = new boolean[vertexTotal];
+			marked = new boolean[vertexTotal];
+			acyclic(i);
+		}
 	}
 	
-	void findLowestCommonAncestor(Node node, int x, int y, Graph adj)
-	{
-		if(Node.data == x) foundXAndY[0] = true;
-		if(Node.data == y) foundXAndY[1] = true;
+	public LowestCommonAncestor reverse() {
+		LowestCommonAncestor reverse = new LowestCommonAncestor(vertexTotal);
+		for (int count = 0; count < vertexTotal; count++) for (int counter : adjacent[count]) reverse.addEdge(counter, count);
+		return reverse;
+	}
+
+	public int LCA(int vertex1, int vertex2) {
+
+		if (!DAG || Y == 0) return -1;
 		
-		int size = adj.getSize(Node.data);
-		
-		if(size == 0) return;
-		
-		for(int i = 0; i < size; i++)
+		boolean commonAncestor = false;
+
+		LowestCommonAncestor reversed = this.reverse();
+		ArrayList<Integer> commonAncestors = new ArrayList<Integer>();
+
+		ArrayList<Integer> branch1 = reversed.BreadthFirstSearch(vertex1);
+		ArrayList<Integer> branch2 = reversed.BreadthFirstSearch(vertex2);
+
+		for (int i = 0; i < branch1.size(); i++) 
 		{
-			findLowestCommonAncestor(adj.getEdge(Node.data, i), x, y, adj);
+			for (int t = 0; t < branch2.size(); t++) 
+			{
+				if (branch1.get(i) == branch2.get(t)) 
+				{
+					commonAncestors.add(branch1.get(i));
+					commonAncestor = true;
+				}
+			}
 		}
+		if (commonAncestor) return commonAncestors.get(0);
+		else return -1;
 	}
-	
-	Graph newGraph(int number) 
-    { 
-        return new Graph(number); 
-    } 
+
+	private ArrayList<Integer> BreadthFirstSearch(int vertex) {
+		boolean isVisited[] = new boolean[vertexTotal];
+		
+		LinkedList<Integer> linkedList = new LinkedList<Integer>();
+		ArrayList<Integer> finalResult = new ArrayList<Integer>();
+
+		linkedList.add(vertex);
+		isVisited[vertex] = true;
+
+		while (linkedList.size() != 0) 
+		{
+			vertex = linkedList.poll();
+			
+			finalResult.add(vertex);
+			Iterator<Integer> iterator = adjacent[vertex].listIterator();
+			
+			while (iterator.hasNext()) 
+			{
+				int n = iterator.next();
+				if (!isVisited[n]) 
+				{ 
+					isVisited[n] = true;
+					linkedList.add(n);
+				}
+			}
+		}
+
+		return finalResult;
+	}
 
 }
